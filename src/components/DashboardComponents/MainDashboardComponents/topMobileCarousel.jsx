b3a9a715bfd49f623bc8a336e8054655ec898087
ihemebiriegondu <ihemebiriegondu@gmail.com>
2023-10-02 15:23:25 +0100

    removed keyboard function

--- src/components/DashboardComponents/MainDashboardComponents/topMobileCarousel.jsx
+++ src/components/DashboardComponents/MainDashboardComponents/topMobileCarousel.jsx
@@ -1,12 +1,9 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination, A11y, Keyboard } from "swiper/modules";
+import { Pagination, A11y} from "swiper/modules";
 
 import "swiper/css";
-import "swiper/css/navigation";
 import "swiper/css/pagination";
-import "swiper/css/scrollbar";
-import "swiper/css/parallax";
 
 import Wallet from "./walletSection";
 import Bank from "./bankSection";
@@ -16,10 +13,9 @@ export default function TopMobileCarousel() {
   return (
     <div>
       <Swiper
-        modules={[Pagination, A11y, Keyboard]}
+        modules={[Pagination, A11y]}
         spaceBetween={50}
         slidesPerView={1}
-        Keyboard={true}
         pagination={{ clickable: true }}
       >
         <CarouselSwipperButtons />
