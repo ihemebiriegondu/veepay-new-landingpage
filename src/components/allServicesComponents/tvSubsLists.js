946e9b680370c6024b747da2031b6067ef9ff0f9
ihemebiriegondu <ihemebiriegondu@gmail.com>
2023-10-12 09:57:54 +0100

    added the tvSubsLists list fro the cable tv page

---
+++ src/components/allServicesComponents/tvSubsLists.js
@@ -0,0 +1,92 @@
+import React from "react";
+
+const TvSubsLists = [
+  {
+    biller: "GOtv",
+    paymentItems: [
+      { item: "Jinja- Monthly", price: "1,900" },
+      { item: "Jolli- Monthly", price: "2,800" },
+      { item: "Max- Monthly", price: "4,150" },
+      { item: "Smallie- Monthly", price: "900" },
+      { item: "SUPA- Monthly", price: "1,900" },
+      { item: "Smallie- Quarterly", price: "2,400" },
+      { item: "Smallie- Annually", price: "7,000" },
+      { item: "Jinja- 4 Months", price: "7,600" },
+      { item: "Jinja- 12 Months", price: "22,800" },
+      { item: "Jolli- 4 Months", price: "2,000" },
+      { item: "Jolli- 12 Months", price: "33,600" },
+      { item: "Max- 4 Month", price: "16,600" },
+      { item: "Max- 12 Months", price: "49,800" },
+      { item: "GOTV RENEW- (Fill Amount)", price: "" },
+      { item: "GOTV TOP UP- (Fill Amount)", price: "" },
+    ],
+  },
+  {
+    biller: "Startimes",
+    paymentItems: [
+      { item: "Basic (Antena) - Weekly", price: "550" },
+      { item: "Basic (Antena) - Monthly", price: "1,700" },
+      { item: "Classic (Antena) - Weekly", price: "1,150" },
+      { item: "Classic (Antena) - Monthly", price: "2,500" },
+      { item: "Nova (Antena) - Weekly", price: "300" },
+      { item: "Nova (Antena) - Monthly", price: "900" },
+      { item: "Solar-Nova - Monthly", price: "9,900" },
+      { item: "Solar-Smart - Monthly", price: "10,500" },
+      { item: "Solar-Super - Monthly", price: "11,400" },
+      { item: "Smart (Dish) - Weekly", price: "700" },
+      { item: "Smart (Dish) - Monthly", price: "2,600" },
+      { item: "Super (Dish) - Weekly", price: "1,500" },
+      { item: "Super (Dish) - Monthly", price: "4,900" },
+      { item: "Nova (Dish) - Weekly", price: "300" },
+      { item: "Nova (Dish) - Monthly", price: "900" },
+      { item: "Startimes All inclusive - Monthly", price: "12,000" },
+      { item: "Startimes Renew - (Fill Amount)", price: "" },
+    ],
+  },
+  {
+    biller: "DSTV",
+    paymentItems: [
+      { item: "Padi - Monthly ", price: "2,150" },
+      { item: "Yanga - Monthly", price: "2,950" },
+      { item: "Confam - Monthly", price: "5,300" },
+      { item: "Compact - Monthly", price: "9,000" },
+      { item: "Compact Plus - Monthly", price: "14,250" },
+      { item: "Premium", price: "21,000" },
+      { item: "Business", price: "2,66g" },
+      { item: "Xtraview + PVR Access", price: "2,900" },
+      { item: "DSTV RENEW (Fill Amount)", price: "" },
+    ],
+  },
+  {
+    biller: "Box Office DSTV",
+    paymentItems: [
+      { item: "Box Office Wallet Topup (Fill Amount)", price: "" },
+    ],
+  },
+  {
+    biller: "SHOWMAX",
+    paymentItems: [
+      { item: "Showmax Mobile - Monthly", price: "1,200" },
+      { item: "Pro Mobile - Monthly", price: "3,200" },
+      { item: "Showmax Pro - Monthly", price: "6,300" },
+      { item: "Showmax - Monthly", price: "2,900" },
+    ],
+  },
+  {
+    biller: "iROKOtv",
+    paymentItems: [
+      { item: "IROKOtv Plus Web Access", price: "800" },
+    ],
+  },
+  {
+    biller: "Linda Ikeji TV",
+    paymentItems: [
+      { item: "1 Month", price: "1,000" },
+      { item: "3 Month", price: "3,000" },
+      { item: "6 Month", price: "5,500" },
+      { item: "12 Month", price: "10,000" },
+    ],
+  },
+];
+
+export default TvSubsLists;
