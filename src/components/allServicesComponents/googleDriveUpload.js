c7f7f9abdfdaa75e93f170b230ffe64b5d4b402a
ihemebiriegondu <ihemebiriegondu@gmail.com>
2023-10-16 00:30:47 +0100

    .

--- src/components/allServicesComponents/googleDriveUpload.js
+++ src/components/allServicesComponents/googleDriveUpload.js
@@ -6,20 +6,36 @@ export default function GoogleDriveUpload() {
   const CLIENT_ID =
     "891792574135-d3d45975advcmsbc8uo2rcuibebacllh.apps.googleusercontent.com";
   const API_KEY = "AIzaSyDD0fA8LuMQKMDs2LoUFDwzcVlc187MKPU";
+  const SCOPE = ["https://www.googleapis.com/auth/drive.readonly"];
 
   return (
     <GooglePicker
       clientId={CLIENT_ID}
       developerKey={API_KEY}
-      scope={["https://www.googleapis.com/auth/drive.readonly"]}
+      scope={SCOPE}
       onChange={(data) => console.log("on change:", data)}
-      onAuthenticate={(token) => console.log("oauth token:", token)}
       onAuthFailed={(data) => console.log("on auth failed:", data)}
       multiselect={true}
       navHidden={true}
       authImmediate={false}
-      mimeTypes={["image/png", "image/jpeg", "image/jpg"]}
-      viewId={"DOCS"}
+      viewId={"FOLDERS"}
+      createPicker={(google, oauthToken) => {
+        const googleViewId = google.picker.ViewId.FOLDERS;
+        const docsView = new google.picker.DocsView(googleViewId)
+          .setIncludeFolders(true)
+          .setMimeTypes("application/vnd.google-apps.folder")
+          .setSelectFolderEnabled(true);
+
+        const picker = new window.google.picker.PickerBuilder()
+          .addView(docsView)
+          .setOAuthToken(oauthToken)
+          .setDeveloperKey(API_KEY)
+          .setCallback(() => {
+            console.log("Custom picker is ready!");
+          });
+
+        picker.build().setVisible(true);
+      }}
     >
       <li className="flex items-center gap-x-2.5 lg:px-10 md:px-6 xs:px-3 px-2 py-px transition duration-100 ease-in-out hover:bg-primary/10 cursor-pointer lg:text-xl md:text-lg sm:text-base xs:text-sm text-xs">
         Upload contacts from Google Drive
