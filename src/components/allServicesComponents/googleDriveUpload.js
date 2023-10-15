6cc316b6400e60141e0eacc52b2aaacd6f8c5b4a
ihemebiriegondu <ihemebiriegondu@gmail.com>
2023-10-15 22:38:18 +0100

    google drive

--- src/components/allServicesComponents/googleDriveUpload.js
+++ src/components/allServicesComponents/googleDriveUpload.js
@@ -1,50 +1,120 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { gapi } from "gapi-script";
 
 export default function GoogleDriveUpload() {
-  const [data, setData] = useState([]);
+  //const [data, setData] = useState([]);
 
-  useEffect(() => {
-    function start() {
-      gapi.client.init({
-        apiKey: "AIzaSyCCd3uDvDOE3m99T58pB1yvzPJNvOUqs54",
-        clientId:
-          "891792574135-nbfadngfq66m846gv327ubmhrls0mn41.apps.googleusercontent.com",
-        scope: "https://www.googleapis.com/auth/drive",
+  const CLIENT_ID =
+    "891792574135-nbfadngfq66m846gv327ubmhrls0mn41.apps.googleusercontent.com";
+  const API_KEY = "AIzaSyCCd3uDvDOE3m99T58pB1yvzPJNvOUqs54";
+  const DISCOVERY_DOCS = [
+    "https://www.googleapis.com/discovery/v1/apis/drive/v3/rest",
+  ];
+  const SCOPES = "https://www.googleapis.com/auth/drive.metadata.readonly";
+
+  const [listDocumentsVisible, setListDocumentsVisibility] = useState(false);
+  const [documents, setDocuments] = useState([]);
+  const [isLoadingGoogleDriveApi, setIsLoadingGoogleDriveApi] = useState(false);
+  const [isFetchingGoogleDriveFiles, setIsFetchingGoogleDriveFiles] =
+    useState(false);
+  const [signedInUser, setSignedInUser] = useState();
+  /*const handleChange = (file) => {
+    console.log(file)
+  };*/
+
+  const listFiles = (searchTerm = null) => {
+    setIsFetchingGoogleDriveFiles(true);
+    gapi.client.drive.files
+      .list({
+        pageSize: 10,
+        fields: "nextPageToken, files(id, name, mimeType, modifiedTime)",
+        q: searchTerm,
+      })
+      .then(function (response) {
+        setIsFetchingGoogleDriveFiles(false);
+        setListDocumentsVisibility(true);
+        const res = JSON.parse(response.body);
+        setDocuments(res.files);
       });
+  };
 
-      gapi.client.load("drive", "v3").then(() => {
-        let response;
+  /**
+   *  Sign in the user upon button click.
+   */
+  const handleAuthClick = (event) => {
+    gapi.auth2.getAuthInstance().signIn();
+  };
 
-        try {
-          response = gapi.client.drive.files.list({
-            pageSize: 10,
-            fields: "files(id, name)",
-          });
-          console.log(response);
-          /*.get({ fileId, alt: "media" })
-            .then((response) => {
-              const fileContent = response.result;
-              setData(fileContent);
-            })
-            .catch((error) => {
-              console.log(error);
-            });*/
-        } catch (error) {
-          console.log(error);
-        }
-        const files = response.result;
-        console.log(files);
-        setData(files)
-      });
+  /**
+   *  Called when the signed in status changes, to update the UI
+   *  appropriately. After a sign-in, the API is called.
+   */
+  const updateSigninStatus = (isSignedIn) => {
+    if (isSignedIn) {
+      // Set the signed in user
+      setSignedInUser(gapi.auth2.getAuthInstance().currentUser.je.Qt);
+      setIsLoadingGoogleDriveApi(false);
+      // list files if user is authenticated
+      listFiles();
+    } else {
+      // prompt user to sign in
+      handleAuthClick();
     }
+  };
 
-    gapi.load("client", start);
-  }, []);
-  console.log(data);
+  /**
+   *  Sign out the user upon button click.
+   */
+ /* const handleSignOutClick = (event) => {
+    setListDocumentsVisibility(false);
+    gapi.auth2.getAuthInstance().signOut();
+  };*/
+
+  /**
+   *  Initializes the API client library and sets up sign-in state
+   *  listeners.
+   */
+  const initClient = () => {
+    setIsLoadingGoogleDriveApi(true);
+    gapi.client
+      .init({
+        apiKey: API_KEY,
+        clientId: CLIENT_ID,
+        discoveryDocs: DISCOVERY_DOCS,
+        scope: SCOPES,
+      })
+      .then(
+        function () {
+          // Listen for sign-in state changes.
+          gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+          // Handle the initial sign-in state.
+          updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+        },
+        function (error) {
+          console.log(error)
+        }
+      );
+  };
+
+  const handleClientLoad = () => {
+    gapi.load("client:auth2", initClient);
+    console.log(documents)
+  };
+
+  /*const showDocuments = () => {
+    setListDocumentsVisibility(true);
+  };
+
+  const onClose = () => {
+    setListDocumentsVisibility(false);
+  };
+*/
+  console.log(documents)
+  console.log(isLoadingGoogleDriveApi, isFetchingGoogleDriveFiles, signedInUser, listDocumentsVisible)
 
   return (
-    <li className="flex items-center gap-x-2.5 lg:px-10 md:px-6 xs:px-3 px-2 py-px transition duration-100 ease-in-out hover:bg-primary/10 cursor-pointer lg:text-xl md:text-lg sm:text-base xs:text-sm text-xs">
+    <li onClick={() => {handleClientLoad()}} className="flex items-center gap-x-2.5 lg:px-10 md:px-6 xs:px-3 px-2 py-px transition duration-100 ease-in-out hover:bg-primary/10 cursor-pointer lg:text-xl md:text-lg sm:text-base xs:text-sm text-xs">
       Upload contacts from Google Drive
     </li>
   );
