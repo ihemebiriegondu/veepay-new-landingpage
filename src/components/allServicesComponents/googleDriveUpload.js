327c34f2f9ca3f15cdb108215a441f766c0ca858
ihemebiriegondu <ihemebiriegondu@gmail.com>
2023-10-15 23:43:07 +0100

    google drive picker

--- src/components/allServicesComponents/googleDriveUpload.js
+++ src/components/allServicesComponents/googleDriveUpload.js
@@ -1,121 +1,49 @@
-import React, { useState } from "react";
-import { gapi } from "gapi-script";
+import React from "react";
+import GooglePicker from "react-google-picker";
 
 export default function GoogleDriveUpload() {
   //const [data, setData] = useState([]);
-
   const CLIENT_ID =
-    "891792574135-nbfadngfq66m846gv327ubmhrls0mn41.apps.googleusercontent.com";
-  const API_KEY = "AIzaSyCCd3uDvDOE3m99T58pB1yvzPJNvOUqs54";
-  const DISCOVERY_DOCS = [
-    "https://www.googleapis.com/discovery/v1/apis/drive/v3/rest",
+    "891792574135-d3d45975advcmsbc8uo2rcuibebacllh.apps.googleusercontent.com";
+  const API_KEY = "AIzaSyDD0fA8LuMQKMDs2LoUFDwzcVlc187MKPU";
+  const APP_ID = "arched-bot-311203";
+  const scope = [
+    "https://www.googleapis.com/auth/drive.readonly",
+    "https://www.googleapis.com/auth/drive.photos.readonly",
   ];
-  const SCOPES = "https://www.googleapis.com/auth/drive.metadata.readonly";
-
-  const [listDocumentsVisible, setListDocumentsVisibility] = useState(false);
-  const [documents, setDocuments] = useState([]);
-  const [isLoadingGoogleDriveApi, setIsLoadingGoogleDriveApi] = useState(false);
-  const [isFetchingGoogleDriveFiles, setIsFetchingGoogleDriveFiles] =
-    useState(false);
-  const [signedInUser, setSignedInUser] = useState();
-  /*const handleChange = (file) => {
-    console.log(file)
-  };*/
-
-  const listFiles = (searchTerm = null) => {
-    setIsFetchingGoogleDriveFiles(true);
-    gapi.client.drive.files
-      .list({
-        pageSize: 10,
-        fields: "nextPageToken, files(id, name, mimeType, modifiedTime)",
-        q: searchTerm,
-      })
-      .then(function (response) {
-        setIsFetchingGoogleDriveFiles(false);
-        setListDocumentsVisibility(true);
-        const res = JSON.parse(response.body);
-        setDocuments(res.files);
-      });
-  };
-
-  /**
-   *  Sign in the user upon button click.
-   */
-  const handleAuthClick = (event) => {
-    gapi.auth2.getAuthInstance().signIn();
-  };
-
-  /**
-   *  Called when the signed in status changes, to update the UI
-   *  appropriately. After a sign-in, the API is called.
-   */
-  const updateSigninStatus = (isSignedIn) => {
-    if (isSignedIn) {
-      // Set the signed in user
-      setSignedInUser(gapi.auth2.getAuthInstance().currentUser.get());
-      setIsLoadingGoogleDriveApi(false);
-      // list files if user is authenticated
-      listFiles();
-    } else {
-      // prompt user to sign in
-      handleAuthClick();
-    }
-  };
-
-  /**
-   *  Sign out the user upon button click.
-   */
- /* const handleSignOutClick = (event) => {
-    setListDocumentsVisibility(false);
-    gapi.auth2.getAuthInstance().signOut();
-  };*/
-
-  /**
-   *  Initializes the API client library and sets up sign-in state
-   *  listeners.
-   */
-  const initClient = () => {
-    setIsLoadingGoogleDriveApi(true);
-    gapi.client
-      .init({
-        apiKey: API_KEY,
-        clientId: CLIENT_ID,
-        discoveryDocs: DISCOVERY_DOCS,
-        scope: SCOPES,
-      })
-      .then(
-        function () {
-          // Listen for sign-in state changes.
-          gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-
-          // Handle the initial sign-in state.
-          updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-        },
-        function (error) {
-          console.log(error)
-        }
-      );
-  };
-
-  const handleClientLoad = () => {
-    gapi.load("client:auth2", initClient);
-    console.log(documents)
-  };
-
-  /*const showDocuments = () => {
-    setListDocumentsVisibility(true);
-  };
-
-  const onClose = () => {
-    setListDocumentsVisibility(false);
-  };
-*/
-  console.log(documents)
-  console.log(isLoadingGoogleDriveApi, isFetchingGoogleDriveFiles, signedInUser, listDocumentsVisible)
 
   return (
-    <li onClick={() => {handleClientLoad()}} className="flex items-center gap-x-2.5 lg:px-10 md:px-6 xs:px-3 px-2 py-px transition duration-100 ease-in-out hover:bg-primary/10 cursor-pointer lg:text-xl md:text-lg sm:text-base xs:text-sm text-xs">
+    <GooglePicker
+      clientId={CLIENT_ID}
+      developerKey={API_KEY}
+      scope={scope}
+      onChange={(data) => console.log("on change:", data)}
+      onAuthFailed={(data) => console.log("on auth failed:", data)}
+      navHidden
+      mimeTypes={["image/png", "image/jpeg", "image/jpg"]}
+      createPicker={(google, oauthToken) => {
+        const picker = new google.picker.PickerBuilder()
+          .addView(new google.picker.View(google.picker.ViewId.DOCS_IMAGES))
+          .addView(new google.picker.DocsUploadView())
+          .setOAuthToken(oauthToken)
+          .setDeveloperKey(API_KEY)
+          .setAppId(APP_ID)
+          .setCallback((data) => {
+            if (data.action === google.picker.Action.PICKED) {
+              var fileId = data.docs[0].id;
+              console.log(fileId)
+            }
+          })
+          .enableFeature(google.picker.Feature.NAV_HIDDEN)
+          .enableFeature(google.picker.Feature.MULTISELECT_ENABLED);
+
+        picker.build().setVisible(true);
+      }}
+    >
+      <button>Continue with google</button>
+      {/* <li className="flex items-center gap-x-2.5 lg:px-10 md:px-6 xs:px-3 px-2 py-px transition duration-100 ease-in-out hover:bg-primary/10 cursor-pointer lg:text-xl md:text-lg sm:text-base xs:text-sm text-xs">
       Upload contacts from Google Drive
-    </li>
+    </li> */}
+    </GooglePicker>
   );
 }
