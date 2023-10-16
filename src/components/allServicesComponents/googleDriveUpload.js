0ee4128f8789802ca3c795b8521bbb51f2c117c2
ihemebiriegondu <ihemebiriegondu@gmail.com>
2023-10-16 01:31:39 +0100

    .

--- src/components/allServicesComponents/googleDriveUpload.js
+++ src/components/allServicesComponents/googleDriveUpload.js
@@ -1,46 +1,43 @@
 import React from "react";
-import GooglePicker from "react-google-picker";
+import useDrivePicker from "react-google-drive-picker";
 
 export default function GoogleDriveUpload() {
   //const [data, setData] = useState([]);
   const CLIENT_ID =
     "891792574135-ifpgmstrb480daqmk2ajitcrgkebjj80.apps.googleusercontent.com";
   const API_KEY = "AIzaSyDD0fA8LuMQKMDs2LoUFDwzcVlc187MKPU";
-  const SCOPE = ["https://www.googleapis.com/auth/drive.readonly"];
+  //const SCOPE = ["https://www.googleapis.com/auth/drive.readonly"];
+
+  const [openPicker, authResponse] = useDrivePicker();
+  // const customViewsArray = [new google.picker.DocsView()]; // custom view
+  const handleOpenPicker = () => {
+    openPicker({
+      clientId: CLIENT_ID,
+      developerKey: API_KEY,
+      viewId: "DOCS",
+      // token: token, // pass oauth token in case you already have one
+      showUploadView: true,
+      showUploadFolders: true,
+      supportDrives: true,
+      multiselect: true,
+      // customViews: customViewsArray, // custom view
+      callbackFunction: (data) => {
+        if (data.action === "cancel") {
+          console.log("User clicked cancel/close button");
+        }
+        console.log(data);
+      },
+    });
+    console.log(authResponse);
+  };
 
   return (
-    <GooglePicker
-      clientId={CLIENT_ID}
-      developerKey={API_KEY}
-      scope={SCOPE}
-      onChange={(data) => console.log("on change:", data)}
-      onAuthFailed={(data) => console.log("on auth failed:", data)}
-      multiselect={true}
-      navHidden={true}
-      authImmediate={false}
-      viewId={"FOLDERS"}
-      createPicker={(google, oauthToken) => {
-        const googleViewId = google.picker.ViewId.FOLDERS;
-        const docsView = new google.picker.DocsView(googleViewId)
-          .setIncludeFolders(true)
-          .setMimeTypes("application/vnd.google-apps.folder")
-          .setSelectFolderEnabled(true);
-
-        const picker = new window.google.picker.PickerBuilder()
-          .addView(docsView)
-          .setOAuthToken(oauthToken)
-          .setDeveloperKey(API_KEY)
-          .setCallback(() => {
-            console.log("Custom picker is ready!");
-          });
-
-        picker.build().setVisible(true);
-      }}
+    <li
+      onClick={() => handleOpenPicker()}
+      className="flex items-center gap-x-2.5 lg:px-10 md:px-6 xs:px-3 px-2 py-px transition duration-100 ease-in-out hover:bg-primary/10 cursor-pointer lg:text-xl md:text-lg sm:text-base xs:text-sm text-xs"
     >
-      <li className="flex items-center gap-x-2.5 lg:px-10 md:px-6 xs:px-3 px-2 py-px transition duration-100 ease-in-out hover:bg-primary/10 cursor-pointer lg:text-xl md:text-lg sm:text-base xs:text-sm text-xs">
-        Upload contacts from Google Drive
-      </li>
-    </GooglePicker>
+      Upload contacts from Google Drive
+    </li>
   );
 }
 
